f5c20efa90226dd70e23753c437c965f3b098a20
vineet2825 <vineetvijay0p@gmail.com>
2026-04-23 18:43:44 +0530

    fix: Add root server.js and update build scripts for Render deployment

---
+++ server.js
@@ -0,0 +1,5 @@
+/**
+ * This file serves as an entry point for platforms like Render 
+ * that may default to looking for a 'server.js' file in the root directory.
+ */
+require('./server/index.js');
